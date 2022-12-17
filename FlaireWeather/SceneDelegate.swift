746eb670d9eb127ed0710406279813b1d6d45a94
Fernando Luiz Goulart <fernando.goulart@exactaworks.com.br>
2022-12-17 10:06:16 -0300

    iOS project setup

---
+++ FlaireWeather/SceneDelegate.swift
@@ -0,0 +1,36 @@
+//
+//  SceneDelegate.swift
+//  FlaireWeather
+//
+//  Copyright © 2022 Leapi. All rights reserved.
+//
+
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = UINavigationController(rootViewController: CityWeatherViewController())
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+    }
+}
---
+++ FlaireWeather/View/CityWeatherViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlaireWeather
+//
+//  Copyright © 2022 Leapi. All rights reserved.
+//
+
+
+import UIKit
+
+class CityWeatherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let view = UIView()
+        view.backgroundColor = .white
+        self.view = view
+    }
+}
+
